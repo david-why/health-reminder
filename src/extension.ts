72e4a0b1f0bd5d8632bec99a512684f13d66c251
David <david_why@outlook.com>
2021-12-05 11:41:56 +0800

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+
+let timer: NodeJS.Timer;
+let statusBar: vscode.StatusBarItem;
+
+export function activate(context: vscode.ExtensionContext) {
+	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBar.hide();
+	timer = setInterval(remind, 1000 * 60);
+	remind();
+	context.subscriptions.push(statusBar);
+}
+
+function remind() {
+	vscode.window.showInformationMessage("Time for your pelvic-floor exercises!");
+	statusBar.text = "Ready?";
+	statusBar.show();
+	let i = 0;
+	for (; i < 20; i++) {
+		setTimeout(() => {
+			statusBar.text = ">o<";
+			statusBar.show();
+		}, 3000 + i * 1000);
+		setTimeout(() => {
+			statusBar.text = ">O<";
+			statusBar.show();
+		}, 3600 + i * 1000);
+	}
+	setTimeout(() => {
+		statusBar.hide();
+	}, 3000 + i * 1000);
+}
+
+export function deactivate() { clearInterval(timer); }
